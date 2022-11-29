1b6f923cd3a65ad9eda76f86d63a6043171a1aa5
anant-jain2011 <kumkumnidhi14@gmail.com>
2022-11-29 21:17:07 +0530

    change1

--- Currency_Converter/scripting.js
+++ Currency_Converter/scripting.js
@@ -2,6 +2,7 @@ const getData = async () => {
 
     let br = document.createElement("br");
     let br2 = document.createElement("br");
+    let br3 = document.createElement("br");
 
     let p = document.createElement("p");
     p.innerHTML = "The result will  <br>    <br>     be shown here";
@@ -20,33 +21,40 @@ const getData = async () => {
     
     let swapImg = document.createElement("img");
     swapImg.src= "https://icon-library.com/images/swap-icon/swap-icon-23.jpg";
-    swapImg.classList.add("swap");
+    swapImg.classList.add("swap2");
 
     let swapBtn = document.createElement("button");
     swapBtn.classList.add("swap");
     swapBtn.append(swapImg);
-
+    
     let select2 = document.createElement("select");
     select2.classList.add("to");
-
+    
     let amt = document.createElement("input");
     amt.placeholder = "Enter Amount here:-";
     amt.type = "number";
     amt.value = 1;
-
+    
+    
     const displayItems = (data, data2) => {
         for (let i = 0; i < data.length; i++) {
             let option = document.createElement("option");
             option.innerHTML = data[i] + ': ' + data2[i];
             option.value = data[i]
-
+            
             let option2 = document.createElement("option");
             option2.innerHTML = data[i] + ': ' + data2[i];
             option2.value = data[i]
-
+            
             select.appendChild(option);
             select2.appendChild(option2);
         }
+    }
+    const swap = () => {
+        let temp = select.value;
+        select.value= select2.value;
+        select2.value= temp;
+        displayResult();
     }
 
     let fetch2 = await fetch("https://api.frankfurter.app/currencies")
@@ -69,6 +77,7 @@ const getData = async () => {
     select.onchange = displayResult;
     select2.onchange = displayResult;
     amt.onkeyup = displayResult;
+    swapBtn.onclick= swap;
 
     amt.classList.add("amount")
 
@@ -80,6 +89,8 @@ const getData = async () => {
 
     document.body.append(li1);
     document.body.append(select);
+    
+    document.body.append(br3);
     
     document.body.append(swapBtn);
 
